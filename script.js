1b2dbee9ecbe18c2ba51f08addf9ca6805da5bdd
Gergely Izsak <izsak.gera@gmail.com>
2021-07-09 18:05:44 +0200

    Finished

--- script.js
+++ script.js
@@ -1,87 +1,103 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var blogSite = [];
-var Locations = /** @class */ (function () {
-    function Locations(city, ZIPcode, address, image) {
+"use strict";
+let blogSite = [];
+class Locations {
+    constructor(city, ZIPcode, address, image, date) {
         this.city = city;
         this.ZIPcode = ZIPcode;
         this.address = address;
         this.image = image;
+        this.date = date;
         blogSite.push(this);
     }
-    Locations.prototype.display = function () {
-        return "\n        \n            <div class=\"card m-5\" style=\"width: 18rem;\">\n                <img class=\"card-img-top img-fluid\" src=\"" + this.image + "\" alt=\"location\">\n                <div class=\"card-body\">\n                    <h5 class=\"card-title\">" + this.city + "</h5>\n                    <p class=\"card-text\">Country: " + this.address + "</p>\n                    <p class=\"card-text\">" + this.ZIPcode + "</p>\n                </div>\n            </div>\n        </div>\n      ";
-    };
-    Locations.prototype.details = function () {
-        return "\n        <a href=\"#\" class=\"btn btn-primary\">Show more</a>";
-    };
-    return Locations;
-}());
-var newLocation1 = new Locations("Chamonix", 74400, "France", "images/Ontwerp-zonder-titel-2021-04-05T191218.134-1024x768.png");
-var newLocation2 = new Locations("Chamonix", 74400, "France", "images/Ontwerp-zonder-titel-2021-04-05T191218.134-1024x768.png");
-var newLocation3 = new Locations("Chamonix", 74400, "France", "images/Ontwerp-zonder-titel-2021-04-05T191218.134-1024x768.png");
-document.getElementById("location").innerHTML += newLocation1.display();
-document.getElementById("location").innerHTML += newLocation2.display();
-document.getElementById("location").innerHTML += newLocation3.display();
-// document.getElementById("btn").addEventListener("click", newLocation.details);
-var Restaurants = /** @class */ (function (_super) {
-    __extends(Restaurants, _super);
-    function Restaurants(city, ZIPcode, address, image, cousineType, webAdress, telNum) {
-        var _this = _super.call(this, city, ZIPcode, address, image) || this;
-        _this.cousineType = cousineType;
-        _this.webAdress = webAdress;
-        _this.telNum = telNum;
-        _this.cousineType = cousineType;
-        _this.webAdress = webAdress;
-        _this.telNum = telNum;
-        return _this;
+    display() {
+        return `
+            <div class="card bg-light border border-dark mb-5" style="width: 18rem;">
+                <img src="${this.image}" style="width: 100%; height: 15vw; object-fit: cover;" class="card-img-top d-none d-sm-none d-md-block d-lg-block d-xl-block" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${this.city}</h5>
+                    <p class="card-text">${this.address}</p>
+                    <p class="card-text">ZIP Code: ${this.ZIPcode}</p>
+      `;
     }
-    Restaurants.prototype.printMore = function () {
-        return " \n            <div class=\"card m-5\" style=\"width: 18rem;\">\n                <img class=\"card-img-top img-fluid\" src=\"" + this.image + "\" alt=\"location\">\n                <div class=\"card-body\">\n                    <h5 class=\"card-title\">" + this.city + "</h5>\n                    <p class=\"card-text\">Country: " + this.address + "</p>\n                    <p class=\"card-text\">Cuisine: " + this.cousineType + "</p>\n                    <p class=\"card-text\">" + this.webAdress + "</p>\n                    <p class=\"card-text\">Tel.:" + this.telNum + "</p>\n                </div>\n            </div>\n\n        ";
-    };
-    return Restaurants;
-}(Locations));
-var newRestaurant1 = new Restaurants("Aiguille Du Midi Restaurant 3842m", 74400, "France", "images/1_18371_02.jpg", "French", "www.e-restaurantnfc.com", "+33 4 50 55 82 23");
-var newRestaurant2 = new Restaurants("Aiguille Du Midi Restaurant 3842m", 74400, "France", "images/1_18371_02.jpg", "French", "www.e-restaurantnfc.com", "+33 4 50 55 82 23");
-var newRestaurant3 = new Restaurants("Aiguille Du Midi Restaurant 3842m", 74400, "France", "images/1_18371_02.jpg", "French", "www.e-restaurantnfc.com", "+33 4 50 55 82 23");
-document.getElementById("restaurant").innerHTML += newRestaurant1.printMore();
-document.getElementById("restaurant").innerHTML += newRestaurant2.printMore();
-document.getElementById("restaurant").innerHTML += newRestaurant3.printMore();
-var Events = /** @class */ (function (_super) {
-    __extends(Events, _super);
-    function Events(city, ZIPcode, address, image, eventDate, eventTime) {
-        var _this = _super.call(this, city, ZIPcode, address, image) || this;
-        _this.eventDate = eventDate;
-        _this.eventTime = eventTime;
-        _this.eventDate = eventDate;
-        _this.eventTime = eventTime;
-        return _this;
+    closingTag() {
+        return `
+            <p class="card-text">Created: ${this.date}</p>
+            </div>
+            </div>
+`;
     }
-    Events.prototype.printMore = function () {
-        return " \n                <div class=\"card m-5\" style=\"width: 18rem;\">\n                    <img class=\"card-img-top img-fluid\" src=\"" + this.image + "\" alt=\"location\">\n                        <div class=\"card-body\">\n                            <h5 class=\"card-title\">" + this.city + "</h5>\n                            <p class=\"card-text\">" + this.eventDate + "</p>\n                            <p class=\"card-text\">" + this.eventTime + "</p>\n                        </div>\n                </div>\n\n        ";
-    };
-    return Events;
-}(Locations));
-var newEvent1 = new Events("Breakfast with a view", 74400, "France", "images/le-3842 (1).jpg", "French", "aiugefiauv");
-var newEvent2 = new Events("Breakfast with a view", 74400, "France", "images/le-3842 (1).jpg", "French", "aiugefiauv");
-var newEvent3 = new Events("Breakfast with a view", 74400, "France", "images/le-3842 (1).jpg", "French", "aiugefiauv");
-document.getElementById("event").innerHTML += newEvent1.printMore();
-document.getElementById("event").innerHTML += newEvent2.printMore();
-document.getElementById("event").innerHTML += newEvent3.printMore();
-for (var _i = 0, blogSite_1 = blogSite; _i < blogSite_1.length; _i++) {
-    var val = blogSite_1[_i];
-    document.getElementById("print").innerHTML += val.display();
 }
+class Restaurants extends Locations {
+    constructor(city, ZIPcode, address, image, date, cousineType, webAdress, telNum) {
+        super(city, ZIPcode, address, image, date);
+        this.cousineType = cousineType;
+        this.webAdress = webAdress;
+        this.telNum = telNum;
+        this.cousineType = cousineType;
+        this.webAdress = webAdress;
+        this.telNum = telNum;
+    }
+    display() {
+        return `
+        ${super.display()}
+        <p class="card-text">Cuisine: ${this.cousineType}</p>
+        <p class="card-text">${this.webAdress}</p>
+        <p class="card-text">${this.telNum}</p>
+      `;
+    }
+}
+class Events extends Locations {
+    constructor(city, ZIPcode, address, image, date, eventDate, eventTime, price) {
+        super(city, ZIPcode, address, image, date);
+        this.eventDate = eventDate;
+        this.eventTime = eventTime;
+        this.price = price;
+        this.eventDate = eventDate;
+        this.eventTime = eventTime;
+        this.price = price;
+    }
+    display() {
+        return ` 
+        ${super.display()}
+        <p class="card-text">Event: ${this.eventDate}</p>
+        <p class="card-text">From: ${this.eventTime}</p>
+        <p class="card-text">Price: ${this.price} €</p>
+        `;
+    }
+}
+var newLocation1 = new Locations("Chamonix", 74400, "France", "images/Ontwerp-zonder-titel-2021-04-05T191218.134-1024x768.png", "May 10, 1980 12:00:00");
+var newLocation2 = new Locations("Bari", 70044, "Italy", "images/DSCF2573.jpg", "Jun 19, 1999 13:25:00");
+var newRestaurant1 = new Restaurants("Aiguille Du Midi Restaurant 3842m", 74400, "France", "images/1_18371_02.jpg", "Feb 10, 1988 2:30:00", "French", "www.e-restaurantnfc.com", "+33 4 50 55 82 23");
+var newRestaurant2 = new Restaurants("Ristorante Grotta Palazzese", 70044, "Italy", "images/grotta-palazzese-restaurant_1.jpg", "Dec 1, 1995 10:50:00", "French", "www.grottapalazzese.it", "+39 080 424 0677");
+var newEvent1 = new Events("Breakfast with a view", 74400, "France", "images/le-3842 (1).jpg", "Sep 30, 1989 11:25:00", "Sep 19 - Sep 21", "08:00 - 10:00", 25);
+var newEvent2 = new Events("Dinner with a view", 70044, "Italy", "images/download.jfif", "Apr 20, 1997 09:50:00", "Apr 25 - Apr 30", "19:00 - 21:00", 35);
+for (let val of blogSite) {
+    document.getElementById("card").innerHTML += val.display() + val.closingTag();
+}
+//////////////////////////////////////////////////////// SORTING BUTTON UP
+function sortUp() {
+    blogSite.sort(function (a, b) {
+        var dateA = new Date(a.date), dateB = new Date(b.date);
+        return dateA - dateB;
+    });
+    var text = "";
+    for (let val of blogSite) {
+        text += val.display() + val.closingTag();
+    }
+    document.getElementById("card").innerHTML = text;
+}
+document.getElementById("sortUp").addEventListener("click", sortUp);
+console.log(blogSite);
+//////////////////////////////////////////////////////// SORTING BUTTON DOWN
+function sortDown() {
+    blogSite.sort(function (a, b) {
+        var dateA = new Date(a.date), dateB = new Date(b.date);
+        return dateB - dateA;
+    });
+    var text = "";
+    for (let val of blogSite) {
+        text += val.display() + val.closingTag();
+    }
+    document.getElementById("card").innerHTML = text;
+}
+document.getElementById("sortDown").addEventListener("click", sortDown);
--- script.ts
+++ script.ts
@@ -1,113 +1,117 @@
 let blogSite: any = [];
 
 class Locations {
-    constructor(public city: string, public ZIPcode: number, public address: string, public image: string) {
+    constructor(public city: string, public ZIPcode: number, public address: string, public image: string, public date: string) {
         blogSite.push(this);
     }
 
     display() {
         return `
-        
-            <div class="card m-5" style="width: 18rem;">
-                <img class="card-img-top img-fluid" src="${this.image}" alt="location">
+            <div class="card bg-light border border-dark mb-5" style="width: 18rem;">
+                <img src="${this.image}" style="width: 100%; height: 15vw; object-fit: cover;" class="card-img-top d-none d-sm-none d-md-block d-lg-block d-xl-block" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${this.city}</h5>
-                    <p class="card-text">Country: ${this.address}</p>
-                    <p class="card-text">${this.ZIPcode}</p>
-                </div>
-            </div>
-        </div>
-      `;
+                    <p class="card-text">${this.address}</p>
+                    <p class="card-text">ZIP Code: ${this.ZIPcode}</p>
+      `
     }
 
-    details() {
+    closingTag() {
         return `
-        <a href="#" class="btn btn-primary">Show more</a>`
+            <p class="card-text">Created: ${this.date}</p>
+            </div>
+            </div>
+`
     }
 }
 
 
-var newLocation1 = new Locations("Chamonix", 74400, "France", "images/Ontwerp-zonder-titel-2021-04-05T191218.134-1024x768.png");
-var newLocation2 = new Locations("Chamonix", 74400, "France", "images/Ontwerp-zonder-titel-2021-04-05T191218.134-1024x768.png");
-var newLocation3 = new Locations("Chamonix", 74400, "France", "images/Ontwerp-zonder-titel-2021-04-05T191218.134-1024x768.png");
-
-document.getElementById("location").innerHTML += newLocation1.display();
-document.getElementById("location").innerHTML += newLocation2.display();
-document.getElementById("location").innerHTML += newLocation3.display();
-
-// document.getElementById("btn").addEventListener("click", newLocation.details);
-
-
 class Restaurants extends Locations {
-    constructor(city: string, ZIPcode: number, address: string, image: string, public cousineType: string, public webAdress: string, public telNum: string) {
-        super(city, ZIPcode, address, image);
+    constructor(city: string, ZIPcode: number, address: string, image: string, date: string, public cousineType: string, public webAdress: string, public telNum: string) {
+        super(city, ZIPcode, address, image, date);
         this.cousineType = cousineType;
         this.webAdress = webAdress;
         this.telNum = telNum;
     }
 
-    printMore() {
-        return ` 
-            <div class="card m-5" style="width: 18rem;">
-                <img class="card-img-top img-fluid" src="${this.image}" alt="location">
-                <div class="card-body">
-                    <h5 class="card-title">${this.city}</h5>
-                    <p class="card-text">Country: ${this.address}</p>
-                    <p class="card-text">Cuisine: ${this.cousineType}</p>
-                    <p class="card-text">${this.webAdress}</p>
-                    <p class="card-text">Tel.:${this.telNum}</p>
-                </div>
-            </div>
-
-        `
+    display() {
+        return `
+        ${super.display()}
+        <p class="card-text">Cuisine: ${this.cousineType}</p>
+        <p class="card-text">${this.webAdress}</p>
+        <p class="card-text">${this.telNum}</p>
+      `;
     }
 }
-
-var newRestaurant1 = new Restaurants ("Aiguille Du Midi Restaurant 3842m", 74400, "France", "images/1_18371_02.jpg", "French", "www.e-restaurantnfc.com", "+33 4 50 55 82 23" );
-var newRestaurant2 = new Restaurants ("Aiguille Du Midi Restaurant 3842m", 74400, "France", "images/1_18371_02.jpg", "French", "www.e-restaurantnfc.com", "+33 4 50 55 82 23" );
-var newRestaurant3 = new Restaurants ("Aiguille Du Midi Restaurant 3842m", 74400, "France", "images/1_18371_02.jpg", "French", "www.e-restaurantnfc.com", "+33 4 50 55 82 23" );
-
-
-document.getElementById("restaurant").innerHTML += newRestaurant1.printMore();
-document.getElementById("restaurant").innerHTML += newRestaurant2.printMore();
-document.getElementById("restaurant").innerHTML += newRestaurant3.printMore();
-
 
 
 class Events extends Locations {
-    constructor(city: string, ZIPcode: number, address: string, image: string, public eventDate: string, public eventTime: string) {
-        super(city, ZIPcode, address, image);
+    constructor(city: string, ZIPcode: number, address: string, image: string, date: string, public eventDate: string, public eventTime: string, public price: number) {
+        super(city, ZIPcode, address, image, date);
         this.eventDate = eventDate;
         this.eventTime = eventTime;
+        this.price = price;
     }
 
-    printMore() {
+    display() {
         return ` 
-                <div class="card m-5" style="width: 18rem;">
-                    <img class="card-img-top img-fluid" src="${this.image}" alt="location">
-                        <div class="card-body">
-                            <h5 class="card-title">${this.city}</h5>
-                            <p class="card-text">${this.eventDate}</p>
-                            <p class="card-text">${this.eventTime}</p>
-                        </div>
-                </div>
-
+        ${super.display()}
+        <p class="card-text">Event: ${this.eventDate}</p>
+        <p class="card-text">From: ${this.eventTime}</p>
+        <p class="card-text">Price: ${this.price} €</p>
         `
     }
 }
 
 
-var newEvent1 = new Events ("Breakfast with a view", 74400, "France", "images/le-3842 (1).jpg", "French", "aiugefiauv");
-var newEvent2 = new Events ("Breakfast with a view", 74400, "France", "images/le-3842 (1).jpg", "French", "aiugefiauv");
-var newEvent3 = new Events ("Breakfast with a view", 74400, "France", "images/le-3842 (1).jpg", "French", "aiugefiauv");
+var newLocation1 = new Locations("Chamonix", 74400, "France", "images/Ontwerp-zonder-titel-2021-04-05T191218.134-1024x768.png", "May 10, 1980 12:00:00");
+var newLocation2 = new Locations("Bari", 70044, "Italy", "images/DSCF2573.jpg", "Jun 19, 1999 13:25:00");
 
+var newRestaurant1 = new Restaurants("Aiguille Du Midi Restaurant 3842m", 74400, "France", "images/1_18371_02.jpg", "Feb 10, 1988 2:30:00", "French", "www.e-restaurantnfc.com", "+33 4 50 55 82 23");
+var newRestaurant2 = new Restaurants("Ristorante Grotta Palazzese", 70044, "Italy", "images/grotta-palazzese-restaurant_1.jpg", "Dec 1, 1995 10:50:00", "French", "www.grottapalazzese.it", "+39 080 424 0677");
 
-document.getElementById("event").innerHTML += newEvent1.printMore();
-document.getElementById("event").innerHTML += newEvent2.printMore();
-document.getElementById("event").innerHTML += newEvent3.printMore();
-
-
+var newEvent1 = new Events("Breakfast with a view", 74400, "France", "images/le-3842 (1).jpg", "Sep 30, 1989 11:25:00", "Sep 19 - Sep 21", "08:00 - 10:00", 25);
+var newEvent2 = new Events("Dinner with a view", 70044, "Italy", "images/download.jfif", "Apr 20, 1997 09:50:00", "Apr 25 - Apr 30", "19:00 - 21:00", 35);
 
 for (let val of blogSite) {
-    (document.getElementById("print") as HTMLElement).innerHTML += val.display();
+    (document.getElementById("card") as HTMLElement).innerHTML += val.display() + val.closingTag();
 }
+
+//////////////////////////////////////////////////////// SORTING BUTTON UP
+
+function sortUp() {
+    blogSite.sort(function(a: string, b: string) {
+        var dateA = new Date(a.date), dateB = new Date(b.date);
+        return dateA - dateB;
+    });
+
+
+var text: string = "";
+
+for (let val of blogSite) {
+    text += val.display() + val.closingTag()
+}
+(document.getElementById("card") as HTMLElement).innerHTML = text;
+}
+
+(document.getElementById("sortUp") as HTMLElement).addEventListener("click", sortUp)
+console.log(blogSite)
+
+//////////////////////////////////////////////////////// SORTING BUTTON DOWN
+
+function sortDown() {
+    blogSite.sort(function (a: string, b: string) {
+        var dateA = new Date(a.date), dateB = new Date(b.date);
+        return dateB - dateA;
+    });
+
+
+var text: string = "";
+
+for (let val of blogSite) {
+    text += val.display() + val.closingTag()
+}
+(document.getElementById("card") as HTMLElement).innerHTML = text;
+}
+
+(document.getElementById("sortDown") as HTMLElement).addEventListener("click", sortDown)
